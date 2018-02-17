58dcc5a2baaeb69a83c7a5da196466f8a1038eb4
mfane <31257747+mfane@users.noreply.github.com>
2018-02-17 18:54:58 -0500

    Personalizes LICENSE and README.md and answers the required questions.

--- exercise1.py
+++ exercise1.py
@@ -11,27 +11,28 @@ I will do the first two lines for you as an example
 
 
 '''
+
 import sys, pygame	# imports the sys and pygame modules so they can be used in this program
 assert sys.version_info >= (3,4), 'This script requires at least Python 3.4' # requires that the Python 3.4 (or higher version) interpreter is being used; i.e., not compatible with Python 2
 
-screen_size = (800,600)
-FPS = 60
+screen_size = (800,600)  #creates a tuple called screen_size
+FPS = 60  #creates a variable called fps
 
-def main():
-	pygame.init()
-	screen = pygame.display.set_mode(screen_size)
-	clock = pygame.time.Clock()
+def main():  #defines a function called main that doesnt take any arguments
+	pygame.init() #initializes pygame
+	screen = pygame.display.set_mode(screen_size)  #makes a screen that is the size of the tuple screen_size
+	clock = pygame.time.Clock() #creates a clock
 
-	while True:
-		clock.tick(FPS)
-		screen.fill((0,0,0))
+	while True: #sets up an infinte loop
+		clock.tick(FPS) #sets the cps to only go as fast as fps
+		screen.fill((0,0,0))  #fills the screen with black
 
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit(0)
+		for event in pygame.event.get(): #sets up a for loop to do something when an event happens
+			if event.type == pygame.QUIT: #checks to see if the event is a quit event
+				pygame.quit() #quits out of pygame
+				sys.exit(0)  #exits out of the system
 
-		pygame.display.flip()
+		pygame.display.flip()  #displays what we have created on the screen to the user all at once
 
-if __name__ == '__main__':
-	main()
+if __name__ == '__main__': #checks to see if this is being run as the main program or is just being called by another .py file
+	main() #runs the main function
--- exercise2.py
+++ exercise2.py
@@ -7,35 +7,46 @@ hint: where you place the screen.fill(black) will affect whether or not the squa
 
 '''
 import sys, pygame, random
-assert sys.version_info >= (3,4), 'This script requires at least Python 3.4' 
 
-screen_size = (800,600)
+assert sys.version_info >= (3, 4), 'This script requires at least Python 3.4'
+
+screen_size = (800, 600)
 FPS = 60
-black = (0,0,0)
-white = (255,255,255)
-red = (255,0,0)
-green = (0,255,0)
-dark_green = (0,128,0)
-yellow = (255,255,0)
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+dark_green = (0, 128, 0)
+yellow = (255, 255, 0)
+
 
 def main():
-	pygame.init()
-	screen = pygame.display.set_mode(screen_size)
-	clock = pygame.time.Clock()
+    pygame.init()
+    screen = pygame.display.set_mode(screen_size)
+    clock = pygame.time.Clock()
+    screen.fill(black)
+    while True:
+        clock.tick(FPS)
+        (x, y, width, height) = (
+            random.randint(0, 800), random.randint(0, 600), random.randint(0, 400), random.randint(0, 300))
+        c = random.randint(0, 4)
+        if c == 0:
+            color = red
+        elif c == 1:
+            color = green
+        elif c == 2:
+            color = white
+        elif c == 3:
+            color = dark_green
+        else:
+            color = yellow
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+        pygame.draw.rect(screen, color, (x, y, width, height))
+        pygame.display.flip()
 
-	(x,y,width,height) = (100,100,50,50)
-
-	while True:
-		clock.tick(FPS)
-
-		screen.fill(black)
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit(0)
-		color = red
-		pygame.draw.rect(screen, color, (x,y,width,height))
-		pygame.display.flip()
 
 if __name__ == '__main__':
-	main()
+    main()
